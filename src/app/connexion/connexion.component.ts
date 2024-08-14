3a73450edb04c992e167fc3060bc73b4e4ba8191
NOUNDJI Aurélien <wazawaaurelien14@gmail.com>
2024-08-14 18:17:54 +0100

    first commit

---
+++ src/app/connexion/connexion.component.ts
@@ -0,0 +1,17 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connexion',
+  standalone: true,
+  imports: [NgFor,
+    NgIf],
+  templateUrl: './connexion.component.html',
+  styleUrl: './connexion.component.css'
+})
+export class ConnexionComponent  {
+
+
+
+
+}
